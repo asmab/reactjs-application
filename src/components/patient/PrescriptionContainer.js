0d14af5dd13f6bf6478f872e961cbed61816b279
asma <asma.boussabat.asma@gmail.com>
2018-05-12 23:31:39 +0200

    prescription page and get search result

--- src/components/patient/PrescriptionContainer.js
+++ src/components/patient/PrescriptionContainer.js
@@ -1,63 +1,58 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 const API_URL = 'https://fest-searcher.herokuapp.com/api/fest/s'
+import Suggestions from './Suggestions'
 
 
-export class PatientListContainer extends React.Component {
+class PrescriptionContainer extends React.Component {
 
     constructor() {
         super();
 
         this.state = {
-          query: ''
+          query: '',
+          results: []
         }
+
+        this.handleInputChange = this.handleInputChange.bind(this)
     }
 
-    componentDidMount() {
-      this.getInfo()
+    handleInputChange() {
+        this.setState({
+          query: this.search.value
+        }, () => {
+
+          if (this.state.query && this.state.query.length > 1) {
+            if (this.state.query.length % 2 === 0) {
+              this.getInfo()
+            }
+          }
+
+        })
     }
-
-
-  handleInputChange () {
-
-    console.log('this search term', this.search)
-
-    this.setState({
-      query: this.search.value
-    }, () => {
-      if (this.state.query && this.state.query.length > 1) {
-        if (this.state.query.length % 2 === 0) {
-          this.getInfo()
-        }
-      } 
-    })
- }
 
    getInfo () {
     axios.get(`${API_URL}/${this.state.query}`)
       .then(({ data }) => {
-
-        console.log('data result ')
         this.setState({
-          results: data.data // MusicGraph returns an object named data, 
-                             // as does axios. So... data.data                             
+          results: data                         
         })
       })
   }
 
- render() {
-   return (
+  render() {
+    return (
       <form>
         <input
           placeholder="Search for..."
           ref={input => this.search = input}
           onChange={this.handleInputChange}
         />
-        <p>{this.state.query}</p>
+        <Suggestions results={this.state.results} />
       </form>
-   )
- }
+    )
+  }
 }
 
-export default PatientListContainer
+export default PrescriptionContainer;
 
